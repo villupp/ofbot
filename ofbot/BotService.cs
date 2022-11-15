4d1915b6864d48de4b37f6dd501b043409c091fc
villupp <v.piirainen@hotmail.com>
2022-11-15 17:43:45 +0200

    Slash commands work in progress.

--- ofbot/BotService.cs
+++ ofbot/BotService.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using Discord.Commands;
+using Discord.Net;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,10 +14,17 @@ namespace OfBot
     {
         private readonly ILogger logger;
         private readonly BotSettings botSettings;
-        private readonly DiscordSocketClient discordSocketClient;
+        private readonly DiscordSocketClient discordClient;
         private readonly CommandService commandService;
         private readonly IServiceProvider serviceProvider;
         private readonly ButtonHandler buttonHandler;
+
+        private static readonly Dictionary<string, string> allGlobalCommands = new()
+        {
+            {"ping", "Get a pong."},
+            {"reg", "Create a registration session."},
+            {"ofbot", "Create a registration session."},
+        };
 
         public BotService(
             ILogger<BotService> logger,
@@ -34,7 +42,7 @@ namespace OfBot
 
             this.logger = logger;
             this.botSettings = botSettings;
-            this.discordSocketClient = discordSocketClient;
+            this.discordClient = discordSocketClient;
             this.commandService = commandService;
             this.serviceProvider = serviceProvider;
             this.buttonHandler = buttonHandler;
@@ -44,25 +52,60 @@ namespace OfBot
         {
             logger.LogInformation("StartAsync");
 
-            discordSocketClient.Log += WriteDiscordLogMessage;
+            discordClient.Log += WriteDiscordLogMessage;
+            discordClient.Ready += OnClientReady;
+            discordClient.ButtonExecuted += buttonHandler.OnButtonExecuted;
 
             if (string.IsNullOrEmpty(botSettings.BotToken))
             {
                 throw new Exception($"Bot token (BotSettings.BotToken) not set in configuration.");
             }
 
-            await discordSocketClient.LoginAsync(TokenType.Bot, botSettings.BotToken);
-            await discordSocketClient.StartAsync();
-
-            discordSocketClient.Ready += () =>
-            {
-                logger.LogInformation("Bot is connected and ready");
-                return Task.CompletedTask;
-            };
-
-            discordSocketClient.ButtonExecuted += buttonHandler.OnButtonExecuted;
+            await discordClient.LoginAsync(TokenType.Bot, botSettings.BotToken);
+            await discordClient.StartAsync();
 
             await InstallCommands();
+        }
+
+        public async Task OnClientReady()
+        {
+            logger.LogInformation("Bot is connected and ready");
+
+            try
+            {
+                await InstallGlobalCommands();
+            }
+            catch (HttpException ex)
+            {
+                logger.LogError($"Error while creating global commands: {ex}");
+            }
+        }
+
+        public async Task InstallGlobalCommands()
+        {
+            var existingGlobalCommands = await discordClient.GetGlobalApplicationCommandsAsync();
+
+            // Add missing global commands
+            foreach (var command in allGlobalCommands)
+            {
+                if (!existingGlobalCommands.Any(c => c.Name.ToLower() == command.Key))
+                {
+                    var globalCommand = new SlashCommandBuilder();
+                    globalCommand.WithName(command.Key);
+                    globalCommand.WithDescription(command.Value);
+
+                    await discordClient.CreateGlobalApplicationCommandAsync(globalCommand.Build());
+                }
+            }
+
+            // Remove unused global commands
+            foreach (var existingCommand in existingGlobalCommands)
+            {
+                if (!allGlobalCommands.ContainsKey(existingCommand.Name.ToLower()))
+                {
+                    await existingCommand.DeleteAsync();
+                }
+            }
         }
 
         private async Task WriteDiscordLogMessage(LogMessage msg)
@@ -76,7 +119,7 @@ namespace OfBot
             logger.LogInformation($"Installing commands");
 
             var messageHandler = serviceProvider.GetService<MessageHandler>();
-            discordSocketClient.MessageReceived += messageHandler.Handle;
+            discordClient.MessageReceived += messageHandler.Handle;
 
             await commandService.AddModulesAsync(
                 assembly: Assembly.GetEntryAssembly(),
